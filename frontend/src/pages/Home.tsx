60119f0e4e0cd17e1693e2ab7b8d8dfc59263540
Simon Zhang <77755696+SimonZhang04@users.noreply.github.com>
2024-09-10 20:46:59 -0400

    updated home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { generateRandomUsername } from "../utils/generateRandomUsername.ts";
 
 export default function Room() {
+  const [username, setUsername] = useState<string>(generateRandomUsername());
+
   return (
     <div>
       <h1 className="text-xxl">Home Page</h1>
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter a username"
+      />
     </div>
   );
 }
--- frontend/src/pages/Room.tsx
+++ frontend/src/pages/Room.tsx
@@ -19,6 +19,7 @@ export default function Room() {
     // Cleanup when component unmounts
     return () => {
       socket.off("message");
+      socket.disconnect();
     };
   }, []);
 
