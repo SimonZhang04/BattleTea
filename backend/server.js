7fc64cb34876cac2fbdbd06f364ba7c3f8d7d214
Simon Zhang <77755696+SimonZhang04@users.noreply.github.com>
2024-09-11 23:42:35 -0400

    fixed socketio for next

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,37 @@
-// backend/server.js
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
 
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const httpServer = http.createServer()
 
-const cors = require('cors');
-
-const app = express();
-const server = http.createServer(app);
-
-app.use(cors({
-  origin: 'http://localhost:3000',    // Same as above, front-end URL
-  credentials: true
-}));
-
-const io = new Server(server, {
+const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:3000',  // Replace with your front-end URL
-    methods: ['GET', 'POST'],         // Allow these HTTP methods
-    credentials: true                 // Allow cookies or other credentials
-  }
-});
+    origin: 'http://localhost:3000', // Replace with your frontend URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+})
 
-const PORT = process.env.PORT || 3001;
-
-// Serve static files if needed (if you want to serve assets from the backend)
-app.get('/', (req, res) => {
-  res.send('Game Server is Running');
-});
-
-// Setup socket.io for real-time communication
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected:', socket.id)
+  socket.on('join_room', (roomId) => {
+    socket.join(roomId)
+    console.log(`user with id-${socket.id} joined room - ${roomId}`)
+  })
 
-   // Send a welcome message to the newly connected client
-   socket.emit('message', 'Welcome to the chat!');
-  
-  // Handle messages or events
-  socket.on('message', (data) => {
-    console.log('Message from client:', data);
-    socket.broadcast.emit('message', data); // Broadcast to other clients
-  });
+  socket.on('send_msg', (data) => {
+    console.log(data, 'DATA')
+    //This will send a message to a specific room ID
+    socket.to(data.roomId).emit('receive_msg', data)
+  })
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+    console.log('A user disconnected:', socket.id)
+  })
+})
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3001
+httpServer.listen(PORT, () => {
+  console.log(`Socket.io server is running on port ${PORT}`)
+})
